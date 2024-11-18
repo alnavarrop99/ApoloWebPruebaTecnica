7ccfe07d7c6dead8239551399b90240f2bd6c111
alnavarrop99 <anavarro.develop99@gmail.com>
2024-11-18 14:12:56 -0500

    core: create alias
    - "~/*": "/src/*"
    - "~/pub/*": "/pub/*"

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+      '~/pub': resolve(__dirname, 'public')
+    }
+  },
   plugins: [react()],
 })
