bb951d55879cbd2c2458f2441b7054e25d50d071
alnavarrop99 <anavarro.develop99@gmail.com>
2024-11-20 04:39:20 -0500

    fix(msw): create and edit characters

--- src/msw/edit.ts
+++ src/msw/edit.ts
@@ -1,5 +1,6 @@
-import { http, HttpResponse } from "msw";
+import { getResponse, http, HttpMethods, HttpResponse } from "msw";
 import * as db from "~/db";
+import { handler } from '.'
 
 export const edit = http.patch<Record<keyof Parameters<typeof db.edit<'character'>>['0']['params'], string>, Parameters<typeof db['edit']>['0']['payload']>(
   `${import.meta.env.APOLO_API_URL}/character` satisfies API_Url<'character'> + '/:id', async ({ params, request }) => {
@@ -7,7 +8,15 @@ export const edit = http.patch<Record<keyof Parameters<typeof db.edit<'character
       const local = JSON.parse(localStorage.db as unknown as string) as typeof localStorage.db
       const req = await request.json()
 
-      const data = Object.assign(local[`${+params.id}`], req)
+      const url = new URL(`${import.meta.env.APOLO_API_URL}/character` satisfies API_Url<'character'> + `/${params.id}`)
+      const res = await getResponse(handler, new Request(url, {
+        method: HttpMethods.GET,
+      }))
+
+      if(!res) throw new Error('params (id) not be available')
+      let data = await res.json()
+
+      data = local[`${+params.id}`] = Object.assign(local?.[`${+params.id}`] ?? data, req)
 
       localStorage.db = JSON.stringify(local) as unknown as typeof localStorage.db
       return HttpResponse.json({ ...data, mock: 'msw active' })
