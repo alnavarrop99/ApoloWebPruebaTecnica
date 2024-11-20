4b02ba6b94dbbd0a1a68f9ff85cc86becdfd3bc8
alnavarrop99 <anavarro.develop99@gmail.com>
2024-11-20 07:43:24 -0500

    feat: create dropdown menu in header

--- src/route/root/nav.tsx
+++ src/route/root/nav.tsx
@@ -8,11 +8,32 @@ export const Nav = ({state, className}: React.ComponentPropsWithRef<'nav'> & { s
       <Link className="btn glass text-xl" to="/"> <img alt='rick and morty image' src={'/R&M.svg'} className="w-8 text-sm" />  <span>R&M</span></Link>
     </div>
     <div className="flex-none">
-      <button className="btn-square glass no-animation" 
-        onClick={() => window.dispatchEvent(new Event('onIconPlay' satisfies keyof WindowEventMap, { bubbles: true }))}
-      >
-        <Icon name="setting" className="w-8" />
-      </button>
+      <details className="dropdown dropdown-end">
+        <summary className="btn btn-square glass no-animation" onClick={() => window.dispatchEvent(new Event('onIconPlay' satisfies keyof WindowEventMap, { bubbles: true }))}><Icon name="setting" className="w-8" /></summary>
+        <ul className="menu dropdown-content bg-gray-100 rounded-box z-[1] w-[12rem] p-2 shadow">
+          <li>
+            <Link className="headless" to="/app/create"
+              onMouseEnter={() => window.dispatchEvent(new Event('onIconPlay' satisfies keyof WindowEventMap, { bubbles: true }))} 
+            >
+              <Icon name="add" trigger="click" className="w-6" />Create character
+            </Link>
+          </li>
+          <li>
+            <Link className="headless" to="/logout" replace
+              onMouseEnter={() => window.dispatchEvent(new Event('onIconPlay' satisfies keyof WindowEventMap, { bubbles: true }))} 
+            >
+              <Icon name="log-out" trigger="click" className="w-6" />Logut
+            </Link>
+          </li>
+          <li>
+            <a href={import.meta.env.DEV ? 'http://localhost:6006/' : import.meta.env.APOLO_STORYBOOK_URL} target="_blank" 
+              onMouseEnter={() => window.dispatchEvent(new Event('onIconPlay' satisfies keyof WindowEventMap, { bubbles: true }))}
+            >
+              <Icon name="note" className="w-6" trigger="click" />Go to sys design
+            </a>
+          </li>
+        </ul>
+      </details>
     </div>
   </section>
   { state !== 'idle' && <progress className="w-full bg-base-100" /> }
