49aaf992a59592a8fe631f0023aab0b8977bce81
alnavarrop99 <anavarro.develop99@gmail.com>
2024-11-20 04:41:58 -0500

    style: set all width in root/main page

--- src/route/root/main.tsx
+++ src/route/root/main.tsx
@@ -1,5 +1,5 @@
 import { twMerge } from "tailwind-merge"
 
-export const Main = ({ className, ...props }: Pick<React.ComponentPropsWithRef<'section'>, 'className' | 'children'>) => <section {...props} aria-label="main content" className={twMerge('grid h-full place-items-center', className)} />
+export const Main = ({ className, ...props }: Pick<React.ComponentPropsWithRef<'section'>, 'className' | 'children'>) => <section {...props} aria-label="main content" className={twMerge('grid h-full items-center', className)} />
 
 export default Main
